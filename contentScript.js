cbe0716efdfe1b488e3dd27a7bf8204c496418e4
Sansarun Sukawongviwat <sansarun@gmail.com>
2015-01-11 11:11:41 +0700

    censor all posts that are loaded before contentScript is executed

--- contentScript.js
+++ contentScript.js
@@ -1,1 +1,24 @@
-console.log("Hello World");
+var wrapperList = document.querySelectorAll(".userContentWrapper");
+
+for(var i=0; i < wrapperList.length; i++) {
+    var wrapper = wrapperList[i];
+    
+    try{
+        var userContent = wrapper.getElementsByClassName("userContent")[0];
+        var message = userContent.firstChild.innerHTML;
+
+        if(shouldCensor(message)) {
+            censor(wrapper);
+        }
+    } catch(e) {
+        console.error(e);
+    }
+}
+
+function shouldCensor(message) {
+    return true;
+}
+
+function censor(element) {
+    element.style.opacity = 0.1;
+}
